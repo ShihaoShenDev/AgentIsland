6d9d9ad3fa97abaa519236ab44fc396bf28ced42
Shihao Shen <shihao@shihaoshen.qzz.io>
2026-07-06 18:41:09 +0800

    fix(tasks): Rename "Run (Debug)" to "Run" and update CIPX package command

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 Write-Host "正在编译..." -ForegroundColor Cyan
 taskkill /im ClassIsland.Desktop.exe /f
-dotnet build
+dotnet build -c Debug
 if ($LASTEXITCODE -eq 0) {
     Write-Host "编译成功，正在启动..." -ForegroundColor Green
     Write-Host "提示：此版本为调试版本，请不要将此版本打包发布。" -ForegroundColor Yellow
