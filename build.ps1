84df7e156d01af5d937d289cedd3dfe93b8207fb
Shihao Shen <shihao@shihaoshen.qzz.io>
2026-07-11 08:30:07 +0800

    chore: Update workflows and scripts - Updated Build scripts and workflows to adapt to the new Solution.

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ dotnet build -c Debug
 if ($LASTEXITCODE -eq 0) {
     Write-Host "编译成功，正在启动..." -ForegroundColor Green
     Write-Host "提示：此版本为调试版本，请不要将此版本打包发布。" -ForegroundColor Yellow
-    & $env:ClassIsland_DebugBinaryDirectory\ClassIsland.Desktop.exe -epp $PWD\bin\Debug\net8.0-windows
+    & $env:ClassIsland_DebugBinaryDirectory\ClassIsland.Desktop.exe -epp $PWD\AgentIsland\bin\Debug\net8.0-windows
 } else {
     Write-Host "编译失败。" -ForegroundColor Red
 }
--- cipx-debug.ps1
+++ cipx-debug.ps1
@@ -1,9 +1,10 @@
 Write-Host "正在创建 ClassIsland 插件包..." -ForegroundColor Cyan
 taskkill /im ClassIsland.Desktop.exe /f
-dotnet publish -c Release -p:CreateCipx=true
+dotnet publish -c Debug -p:CreateCipx=true
 if ($LASTEXITCODE -eq 0) {
-    Write-Host "ClassIsland 插件包创建成功! 位置: $PWD\cipx" -ForegroundColor Green
+    Write-Host "ClassIsland 插件包创建成功! 位置: $PWD\AgentIsland\cipx" -ForegroundColor Green
     Write-Host "请参考 https://docs.classisland.tech/dev/plugins/publishing.html 来发布插件。" -ForegroundColor Yellow
-} else {
+}
+else {
     Write-Host "ClassIsland 插件包创建失败。" -ForegroundColor Red
 }
